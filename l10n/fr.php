3e78343dc9fd37cee950673f9eff7beb1a108e1d
Patrick Paysant <patrick.paysant@linagora.com>
2015-10-07 11:44:23 +0200

    Fix translations

--- l10n/fr.php
+++ l10n/fr.php
@@ -1,11 +1,12 @@
 <?php
 $TRANSLATIONS = array(
+'Welcome to %s' => 'Bienvenue dans %s',
 'Local My CoRe password' => "Mot de passe My CoRe local",
-"Local My CoRe password Policy Enforcement" => "Stratégie de choix du mot de passe local My CoRe",
+"Local My CoRe password policy enforcement" => "Stratégie de choix du mot de passe local My CoRe",
 "New password" => "Nouveau mot de passe",
 "Confirm password" => "Confirmer le mot de passe",
 "Change password" => "Changer le mot de passe",
-"You're presently connected via \"Janus\", if you want to use sync soft or mobile app, please set a password (this password will be exclusively used with My CoRe)." => "Vous êtes actuellement connecté via \"Janus\", Si vous souhaitez utiliser le logiciel de synchronisation ou une application mobile, merci d'initialiser un mot de passe (ce mot de passe est destiné uniquement au service My CoRe.",
+"You're presently connected through \"Janus\", if you want to use synchronization software or mobile app, please set a password (this password will be exclusively used with My CoRe)." => "Vous êtes actuellement connecté via \"Janus\", si vous souhaitez utiliser le logiciel de synchronisation ou une application mobile, merci d'initialiser un mot de passe (ce mot de passe est destiné uniquement au service My CoRe).",
 "Skip this step, I will set this local My CoRe password later." => "Je souhaite passer cette étape, je pourrai initialiser un mot de passe local à My CoRe plus tard.",
 "The following password restrictions are currently in place:" => "Les contraintes sur le choix d'un mot de passe sont les suivantes :",
 "All passwords are required to be at least %d characters in length and;" => "Chaque mot de passe doit contenir au moins %d caractères et ",
@@ -14,5 +15,8 @@ $TRANSLATIONS = array(
 "Must contain special characters: %s" => "Doit contenir au moins un des caractères particuliers suivants : %s",
 'Password successfully changed' => "Le mot de passe a été enregistré",
 "Unable to change password" => "Échec à la modification de votre mot de passe",
+'Passwords differs.' => 'Les mots de passe sont différents.',
+'Both password fields must be set.' => 'Les deux champs \"mot de passe\" doivent être renseignés.',
+''
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
